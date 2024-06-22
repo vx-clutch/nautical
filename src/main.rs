0c38f2bd078551b40043ff2fcd89bc4efe59112c
vx-clutch <owestness@gmail.com>
2024-06-22 19:29:50 -0400

    save

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use clap::{Parser, ValueEnum};
-
 enum Token {
     IDENT,
     IntLit,
@@ -14,25 +12,4 @@ enum Token {
     LBRACKET,
 }
 
-enum Mode {
-    Friendly,
-    Formal,
-}
-
-#[derive(ValueEnum, Debug, Clone)]
-struct Args {
-    name: String,
-    count: u32,
-    mode: Mode,
-}
-
-fn main() {
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        match args.mode {
-            Mode::Friendly => println!("Hello, {}!", args.name),
-            Mode::Formal => println!("Good day, {}.", args.name),
-        }
-    }
-}
+fn main() {}
