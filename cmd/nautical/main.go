585baa81b770b615580d398f8aa3d3ecb643ad0f
vx-clutch <owestness@gmail.com>
2024-06-23 16:35:05 -0400

    save

--- cmd/nautical/main.go
+++ cmd/nautical/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"nautical/pkg/compiler"
+	"os"
+	"strings"
+)
 
+func main() {
+	commandChecks()
+	filename := os.Args[1]
+	defaultPath := strings.TrimSuffix(filename, ".nm")
+	fmt.Println(defaultPath)
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	compiler.Compiler(content)
+}
+
+func commandChecks() {
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	} // Checks for arguments
+	if strings.HasSuffix(os.Args[1], ".hm") {
+		fmt.Println("error: invalid file extention\n\tcorrent usage: <filename>.nm")
+	} // Checks for proper file extention
 }
